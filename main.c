fad11adf512661f9f5bbf279f45106778b29a5f8
Joel <jggrif@student.unimelb.edu.au>
2022-05-09 11:33:20 +1000

    Added socket wrapper

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 /* Socket setup based on the examples in "Beej's Guide to Network Programming": https://beej.us/guide/bgnet/html/ */
 
+#include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
@@ -7,9 +8,26 @@
 #include <sys/socket.h>
 #include <netdb.h>
 
-#define BACKLOG 5
+#define BACKLOG 10 
+#define BUFFER_LEN 256
+
+struct sockwrap {
+	int sockfd, buff_len, start, n;
+	char *buff;
+};
+
+struct sockwrap *init_sockwrap(int sockfd, int buff_len);
+int freesockwrap(struct sockwrap *sockw);
+int nextchar(struct sockwrap *sockw, char *c);
 
 int main(int argc, char **argv) {
+	/* Validate arguments */
+	if (argc != 4) {
+		fprintf(stderr, "usage: %s [protocol number] [port number] [web root]\n", argv[0]);
+		return 1;
+	}
+	// TODO: validate arguments... helper function?
+	char *protocol = argv[1], *port = argv[2], *root = argv[3];
 	struct addrinfo hints, *res;
 
 	/* Specify interface options */
@@ -20,7 +38,7 @@ int main(int argc, char **argv) {
 
 	/* Get interfaces which adhere to our options */
 	int status;
-	if ((status = getaddrinfo(NULL, "8080", &hints, &res)) != 0) {
+	if ((status = getaddrinfo(NULL, port, &hints, &res)) != 0) {
 		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(status));
 		return 1;
 	}
@@ -80,6 +98,51 @@ int main(int argc, char **argv) {
 		return 1;
 	}
 	
+	// struct sockwrap *sockw = init_sockwrap(newsockfd, BUFFER_LEN);
+	// freesockwrap(sockw);
 	close(sockfd);
 	return 0;
+}
+
+struct sockwrap *init_sockwrap(int sockfd, int buff_len) {
+	struct sockwrap *sockw = (struct sockwrap *)malloc(sizeof(struct sockwrap));
+	if (sockw == NULL) {
+		return NULL;
+	}
+
+	char *buff = (char *)malloc(buff_len * sizeof(char));
+	if (buff == NULL) {
+		free(sockw);
+		return NULL;
+	}
+
+	sockw->sockfd = sockfd;
+	sockw->buff_len = buff_len;
+	sockw->start = 0;
+	sockw->n = 0;
+	sockw->buff = buff;
+	return sockw;
+}
+
+int freesockwrap(struct sockwrap *sockw) {
+	int sockfd = sockw->sockfd;
+	free(sockw->buff);
+	free(sockw);
+	return sockfd;
+}
+
+int nextchar(struct sockwrap *sockw, char *c) {
+	if (sockw->n == 0) {
+		int n = recv(sockw->sockfd, sockw->buff, sockw->buff_len, 0);
+		if (n == -1 || n == 0) { 
+			return n;
+		}
+		sockw->start = 0;
+		sockw->n = n;
+	}
+	
+	*c = sockw->buff[sockw->start];
+	sockw->start++; // TODO: should we leave this index out of range if we read the entire buffer?
+	sockw->n--;
+	return 1;
 }
