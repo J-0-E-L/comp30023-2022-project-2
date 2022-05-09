1d8aee24d8d10b7755ad3bedca4f81cd63781692
Joel <jggrif@student.unimelb.edu.au>
2022-05-09 14:01:39 +1000

    Added get next token

--- main.c
+++ main.c
@@ -10,6 +10,7 @@
 
 #define BACKLOG 10 
 #define BUFFER_LEN 256
+#define TOKEN_LEN 64
 
 struct sockwrap {
 	int sockfd, buff_len, start, n;
@@ -19,6 +20,7 @@ struct sockwrap {
 struct sockwrap *init_sockwrap(int sockfd, int buff_len);
 int freesockwrap(struct sockwrap *sockw);
 int nextchar(struct sockwrap *sockw, char *c);
+int nexttoken(struct sockwrap *sockw, char **s, int token_len, char del);
 
 int main(int argc, char **argv) {
 	/* Validate arguments */
@@ -98,8 +100,21 @@ int main(int argc, char **argv) {
 		return 1;
 	}
 	
-	// struct sockwrap *sockw = init_sockwrap(newsockfd, BUFFER_LEN);
-	// freesockwrap(sockw);
+	struct sockwrap *sockw = init_sockwrap(newsockfd, BUFFER_LEN);
+	
+	while (1) {
+		char *s;
+		int n = nexttoken(sockw, &s, TOKEN_LEN, '\n');
+		if (n == 1) {
+			printf("%s\n", s);
+			free(s);
+			fflush(stdout);
+		} else {
+			break;
+		}
+	}
+
+	freesockwrap(sockw);
 	close(sockfd);
 	return 0;
 }
@@ -131,7 +146,9 @@ int freesockwrap(struct sockwrap *sockw) {
 	return sockfd;
 }
 
+/* -1: error, 0: disconnect, 1: character received */
 int nextchar(struct sockwrap *sockw, char *c) {
+	/* Get more data when the buffer is empty */
 	if (sockw->n == 0) {
 		int n = recv(sockw->sockfd, sockw->buff, sockw->buff_len, 0);
 		if (n == -1 || n == 0) { 
@@ -141,8 +158,46 @@ int nextchar(struct sockwrap *sockw, char *c) {
 		sockw->n = n;
 	}
 	
+	/* Return the first character in the buffer */
 	*c = sockw->buff[sockw->start];
 	sockw->start++; // TODO: should we leave this index out of range if we read the entire buffer?
 	sockw->n--;
+	return 1;
+}
+
+/* -1: error, 0: disconnect, 1: token received, 2: token too large */
+int nexttoken(struct sockwrap *sockw, char **s, int token_len, char del) { // TODO: support multi-characer delimeters
+	char *t = (char *)malloc((token_len + 1) * sizeof(char)); // TODO: use a buffer instead of allocating each time
+	if (t == NULL) {
+		return -1;
+	}
+	
+	int i, found = 0;
+	char c;
+	for (i = 0; i < token_len + 1; i++) {
+		/* Get the next character */
+		int n = nextchar(sockw, &c);
+		if (n == -1 || n == 0) {
+			return n;
+		}
+
+		/* Found the delimeter */
+		if (c == del) {
+			t[i] = '\0';
+			found = 1;
+			break;
+		}
+		/* Still in the same token */
+		t[i] = c;
+	}
+
+	/* The token received is too big */
+	if (!found) {
+		free(t);
+		return 2;
+	}
+
+	t = (char *)realloc(t, (i + 1) * sizeof(char));
+	*s = t;
 	return 1;
 }
