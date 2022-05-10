4af5f07cb82015f00abbadd4acb8509a489a1e3e
Joel <jggrif@student.unimelb.edu.au>
2022-05-10 14:17:34 +1000

    Fixed send response body

--- main.c
+++ main.c
@@ -21,92 +21,24 @@ struct sockwrap *initsockwrap(int sockfd, int buff_len);
 int freesockwrap(struct sockwrap *sockw);
 int nextchar(struct sockwrap *sockw, char *c);
 int nexttoken(struct sockwrap *sockw, char *token, int token_len, int *token_n, char *del);
+int sendfile(struct sockwrap *sockw, FILE *fp);
+
+int getargs(int argc, char **argv, char **protocol, char **port, char **root);
+int getlistensock(int *sockfd, char *protocol, char *port);
 
 int validpath(char *path);
 char *trap(char *root, char *path);
 
 int main(int argc, char **argv) {
-	/* Validate arguments */
-	if (argc != 4) {
-		fprintf(stderr, "usage: %s [protocol number] [port number] [web root]\n", argv[0]);
+	/* Read and verify command line arguments */
+	char *protocol, *port, *root;
+	if (getargs(argc, argv, &protocol, &port, &root) == 1) {
 		return 1;
 	}
 
-	char *protocol = argv[1], *port = argv[2], *root = argv[3];
-	
-	if (strcmp(protocol, "6") != 0 && strcmp(protocol, "4") != 0) {
-		fprintf(stderr, "protocol number must be 4 or 6\n");
-		return 1;
-	}
-	
-	int t = atoi(port); // TODO: do not use atoi
-	if (t < 0 || t >= 65536) {
-		fprintf(stderr, "port must be between 0 and 65535 (inclusive)\n");
-		return 1;
-	}
-
-	if (!validpath(root)) { // TODO: does the folder exist?
-		fprintf(stderr, "web root must be a valid path\n");
-		return 1;
-	}
-
-	struct addrinfo hints, *res;
-
-	/* Specify interface options */
-	memset(&hints, 0, sizeof(struct addrinfo));
-	// hints.ai_family = AF_UNSPEC;
-	hints.ai_family = (strcmp(protocol, "6") == 0) ? AF_INET6 : AF_INET;
-	hints.ai_socktype = SOCK_STREAM;
-	hints.ai_flags = AI_PASSIVE;
-
-	/* Get interfaces which adhere to our options */
-	int status;
-	if ((status = getaddrinfo(NULL, port, &hints, &res)) != 0) {
-		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(status));
-		return 1;
-	}
-
-	/* Bind to the first interface we can */
-	struct addrinfo *p;
+	/* Produce a listening socket */
 	int sockfd;
-	for (p = res; p; p = p->ai_next) {
-		/* Try to get a suitable socket for this interface */
-		if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
-			perror("socket");
-			continue;
-		}
-		
-		/* Enable immediate reuse of the interface and port */
-		int enable = 1;
-		if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int)) == -1) {
-			perror("setsockopt");
-			close(sockfd);
-			freeaddrinfo(res);
-			return 1;
-		}
-
-		/* Try to bind the socket to a port */
-		if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
-			perror("bind");
-			close(sockfd);
-			continue;
-		}
-
-		/* We have a valid socket */
-		break;
-	}
-	freeaddrinfo(res);
-	
-	/* None of the interfaces worked */
-	if (p == NULL) {
-		fprintf(stderr, "server: failed to bind\n");
-		return 1;
-	}
-	
-	/* Get ready to receive connections */
-	if (listen(sockfd, BACKLOG) == -1) {
-		perror("listen");
-		close(sockfd);
+	if (getlistensock(&sockfd, protocol, port) == 1) {
 		return 1;
 	}
 
@@ -123,56 +55,51 @@ int main(int argc, char **argv) {
 		
 		struct sockwrap *sockw = initsockwrap(newsockfd, BUFFER_LEN);
 
-		/* Request line */
-		// TODO: package format into a nice struct
+		/* Read in the request line */
 		char method[TOKEN_LEN + 1], file[TOKEN_LEN + 1], version[TOKEN_LEN + 1];
-		int token_n;
-		if (nexttoken(sockw, method, TOKEN_LEN, &token_n, " ") == -1) {
-			perror("nexttoken");
-			return 1;
+		char *tokens[3] = {method, file, version}, *dels[3] = {" ", " ", "\r\n"}; // tokens and dels must be the same length
+		int tokens_len = 3;
+		for (int i = 0; i < tokens_len; i++) {
+			int token_n;
+			if (nexttoken(sockw, tokens[i], TOKEN_LEN, &token_n, dels[i]) == -1) {
+				perror("nexttoken");
+				freesockwrap(sockw);
+				close(newsockfd);
+				close(sockfd);
+				return 1;
+			}
+			tokens[i][token_n] = '\0';
 		}
-		method[token_n] = '\0';
 
-		if (nexttoken(sockw, file, TOKEN_LEN, &token_n, " ") == -1) {
-			perror("nexttoken");
-			return 1;
-		}
-		file[token_n] = '\0';
-
-		if (nexttoken(sockw, version, TOKEN_LEN, &token_n, "\r\n") == -1) {
-			perror("nexttoken");
-			return 1;
-		}
-		version[token_n] = '\0';
-
+		// TODO: at least look at the headers
+	
+		/* Serve the request */
 		char *msg;
-		// printf("method: %s\n", method);
-		// printf("file: %s\n", file);
-		// printf("version: %s\n", version);
-
 		if (strcmp(method, "GET") != 0 || validpath(file) == 0 || strcmp(version, "HTTP/1.0") != 0) {
 			msg = "HTTP/1.0 400 Invalid request\r\n\r\n";
 			send(sockw->sockfd, msg, strlen(msg), 0);
 		} else {
 			char *trapped = trap(root, file);
+			if (trapped == NULL) {
+				freesockwrap(sockw);
+				close(newsockfd);
+				close(sockfd);
+				return 1;
+			}
+
 			FILE *fp = fopen(trapped, "r");
 			free(trapped);
-			if (fp == NULL) {
+			if (fp == NULL) { // TODO: implement code 403 (read permissions... what level should the program need?)
 				msg = "HTTP/1.0 404 File not found\r\n\r\n";
 				send(sockw->sockfd, msg, strlen(msg), 0);
 			} else {
 				msg = "HTTP/1.0 200 OK\r\n\r\n";
 				send(sockw->sockfd, msg, strlen(msg), 0);
-				
-				char buff[20];
-				int nread; 
-				while ((nread = fread(buff, sizeof(char), 20, fp)) > 0) {
-					send(sockw->sockfd, buff, nread, 0);
-				}
+				sendfile(sockw, fp);
 				fclose(fp);
 			}
 		}
-		
+
 		freesockwrap(sockw);
 		close(newsockfd);
 	}
@@ -264,6 +191,121 @@ int nexttoken(struct sockwrap *sockw, char *token, int token_len, int *token_n, 
 	}
 	/* We should never get here */
 	return -1;
+}
+
+/* -1: error, 1: file sent */
+int sendfile(struct sockwrap *sockw, FILE *fp) {
+	char buff[BUFFER_LEN]; // TODO: should sockwrap contain an out buffer?
+	int nread;
+	while ((nread = fread(buff, sizeof(char), BUFFER_LEN, fp)) > 0) { // TODO: is it dangerous to use a 0 character read to indicate EOF?
+		int ntosend = nread;
+		/* Make sure everything is sent */
+		while (ntosend > 0) {
+			int nsent = send(sockw->sockfd, buff, nread, 0);
+			if (nsent == -1) {
+				perror("sendfile");
+				return -1;
+			}
+			ntosend -= nsent;
+		}
+	}
+	// TODO: is there a way of detecting errors in fread?
+	return 1;
+}
+
+int getargs(int argc, char **argv, char **protocol, char **port, char **root) {
+	if (argc != 4) {
+		fprintf(stderr, "usage: %s [protocol number] [port number] [web root]\n", argv[0]);
+		return 1;
+	}
+
+	*protocol = argv[1];
+	*port = argv[2];
+	*root = argv[3];
+	
+	if (strcmp(*protocol, "6") != 0 && strcmp(*protocol, "4") != 0) {
+		fprintf(stderr, "protocol number must be 4 or 6\n");
+		return 1;
+	}
+	
+	int t = atoi(*port); // TODO: do not use atoi
+	if (t < 0 || t >= 65536) {
+		fprintf(stderr, "port must be between 0 and 65535 (inclusive)\n");
+		return 1;
+	}
+
+	if (!validpath(*root)) { // TODO: does the folder exist?
+		fprintf(stderr, "web root must be a valid path\n");
+		return 1;
+	}
+
+	return 0;
+}
+
+int getlistensock(int *sockfd, char *protocol, char *port) {
+	struct addrinfo hints, *res;
+
+	/* Specify interface options */
+	memset(&hints, 0, sizeof(struct addrinfo));
+	// hints.ai_family = AF_UNSPEC;
+	hints.ai_family = (strcmp(protocol, "6") == 0) ? AF_INET6 : AF_INET;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_flags = AI_PASSIVE;
+
+	/* Get interfaces which adhere to our options */
+	int status;
+	if ((status = getaddrinfo(NULL, port, &hints, &res)) != 0) {
+		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(status));
+		return 1;
+	}
+
+	/* Bind to the first interface we can */
+	struct addrinfo *p;
+	int newsockfd;
+	for (p = res; p; p = p->ai_next) {
+		/* Try to get a suitable socket for this interface */
+		if ((newsockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
+			perror("socket");
+			continue;
+		}
+		
+		/* Enable immediate reuse of the interface and port */
+		int enable = 1;
+		if (setsockopt(newsockfd, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int)) == -1) {
+			perror("setsockopt");
+			close(newsockfd);
+			freeaddrinfo(res);
+			return 1;
+		}
+
+		/* Try to bind the socket to a port */
+		if (bind(newsockfd, p->ai_addr, p->ai_addrlen) == -1) {
+			perror("bind");
+			close(newsockfd);
+			continue;
+		}
+
+		/* We have a valid socket */
+		break;
+	}
+	freeaddrinfo(res);
+	
+	/* None of the interfaces worked */
+	if (p == NULL) {
+		fprintf(stderr, "server: failed to bind\n");
+		return 1;
+	}
+	
+	/* Get ready to receive connections */
+	if (listen(newsockfd, BACKLOG) == -1) {
+		perror("listen");
+		close(newsockfd);
+		return 1;
+	}
+	
+	/* Expose the listening socket */
+	*sockfd = newsockfd;
+	return 0;
 }
 
 int validpath(char *path) { // TODO: implement this
